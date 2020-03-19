0855805fff1d6762a8f1223e9a51739b6dde72f8
User <s9nlscha@stud.uni-saarland.de>
2020-03-19 16:46:40 +0100

    moved bigger TODOS to document, removed deprecated filtering

--- distinct_reports_lines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-files=$(find ./reports -type f)
-for i in $files
-do
-  cat $i | uniq > tmp4242 
-  rm $i
-  mv tmp4242 $i
-done
--- tracker/src/main/java/boundarydetection/tracker/AccessTracker.java
+++ tracker/src/main/java/boundarydetection/tracker/AccessTracker.java
@@ -6,41 +6,6 @@ import java.util.Random;
 
 public class AccessTracker {
 
-    //TODO it would be useful to have a no-false-positives and a no-false-negative mode
-    // --> no-false-positives only gives sensible boundaries that need to be supported, but could miss some cases
-    //     no-false-negatives is the other way round, complete but much
-    // no-false-positives:  do not support array as fields (stretching),
-    //                      do not consider writes of newly created objects in constructors (init cases)
-    //TODO support field access if the object is an array. that gives a false positive everytime the array
-    // is streched, but this could lead to false negatives, so we should have an option to
-    // switch it on
-    //TODO mark fieldwriter with readers, if we already saw this reader reading from the same writer case, do not report again (currently solved by distinct.py)
-    //TODO refactor, keep complexity of redundancy recognition outside of basic recognition
-    //TODO reading and writing an object to another array index or another array (copied), does
-    // not loose old writers, if we associate writers with an object.
-    // but we have to recognize positions when a writer overtake is sensible. If the copier writes the object, it will take the place of the old writer
-    // Load balancing of requests over workers gives false positives, if we do it every time.
-    // So only if we copy an array that ends up being in the same location as the old one, but this is not trivial to detect.
-    // -> for now it is ok to rely on the nondeterminism argument when arrays are copied.
-    // easy way for refactoring to object perspective-> We currently look at accesses to ArrayFields or Fields (represent locations) and track accesses to them
-    // we just have to redefine what an access location is. Instead of fields and arrays we say objects if we have
-    // an object at hand or the field location (array index, field) if it is a primitive.
-
-    //TODO use weak refs to get rid of locations (relative to objects) that are dead and should be destroyed
-    // we have to take care of access counters, if we destroy dynamic locations and want to preserve code locations
-    // -> just mark them as archived and destroy parent but keep the rest
-
-    // TODO REFACTOR: Accesscontroller as accesspoint from outside
-    // Access tracker as buisness tracker model that makes the decisions, move code from field access meta to tracker
-    // field access meta as pure datastructure/infrastructure
-
-    //TODO support for fields with other types than object e.g. for array as field (access of the array object)
-    //TODO bytecode instrumentation, related problems and solution document
-
-    //TODO performance: allow more interleaving, task related things can run under a different lock, comes with refactoring
-    //TODO refactor inits
-    //TODO move task logic to other class
-
     private static final int MAP_INIT_SIZE = 10000;
 
     private static HashMap<AbstractFieldLocation, FieldAccessMeta> accesses;
@@ -85,8 +50,6 @@ public class AccessTracker {
             synchronized (AccessTracker.class) {
                 insideTracker.set(true);
                 if (!hasTask()) return;
-                // System.out.println(Util.toString(Thread.currentThread().getStackTrace()));
-                // if (!XTraceBaggageInterface.hasTaskID()) return;
                 FieldAccessMeta meta = accesses.get(f);
                 if (meta == null) {
                     meta = new FieldAccessMeta();
@@ -113,7 +76,6 @@ public class AccessTracker {
         try {
             synchronized (AccessTracker.class) {
                 insideTracker.set(true);
-                // System.out.println("READ: " + toString(Thread.currentThread().getStackTrace()));
                 FieldAccessMeta meta = accesses.get(field);
                 if (meta == null) return;
 
@@ -155,9 +117,6 @@ public class AccessTracker {
         if (pausedTaskCounter.get() == null) pausedTaskCounter.set(0);
 
         int c = pausedTaskCounter.get();
-//        FOR DEBUG
-//        for (int i = 0; i < c; i++) System.out.print(" ");
-//        System.out.println("(");
         if (c == 0) {
             pausedTask.set(task.get());
             task.remove();
@@ -173,9 +132,6 @@ public class AccessTracker {
         if (c == 0) {
             task.set(pausedTask.get());
         }
-//        FOR DEBUG
-//        for (int i = 0; i < c; i++) System.out.print(" ");
-//        System.out.println(")");
     }
 
     private static synchronized void initTaskLocals() {
